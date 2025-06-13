35091a8302663089d8b9ee01c5fbe97990139b5a
AsocPro <asocpro@gmail.com>
2025-06-13 06:22:23 +0000

    generate AUTH_SECRET and simplify the container image

--- habit_trove_addon/run.sh
+++ habit_trove_addon/run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+
+# Check if /app/data/.env exists, if not, generate AUTH_SECRET and create it
+if [ ! -f /app/data/.env ]; then
+    AUTH_SECRET=$(openssl rand -base64 32)
+    echo "export AUTH_SECRET=$AUTH_SECRET" > /app/data/.env
+fi
+
+source /app/data/.env
+
 cd /app
-npm start
+node server.js
